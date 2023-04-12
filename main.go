2c649998030d46819b6c0618e569205cef47593f
Abhimanyu Sharma <71812498+0xN1nja@users.noreply.github.com>
2023-04-13 01:12:51 +0530

    Added Search Functionality

--- main.go
+++ main.go
@@ -1,11 +1,44 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/ttacon/chalk"
 )
 
+func search(query string, file_path string) []string {
+	chalk.Red.Color("as")
+	file, err := os.Open(file_path)
+	if err != nil {
+		fmt.Println("Unable To Read File")
+		os.Exit(1)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	var results []string
+	for scanner.Scan() {
+		file_content := scanner.Text()
+		for _, lines := range strings.Split(file_content, "\n") {
+			if strings.Contains(lines, query) {
+				results = append(results, strings.ReplaceAll(lines, query, chalk.Red.Color(query)))
+			}
+		}
+	}
+	return results
+}
+
 func main() {
-	fmt.Println(chalk.Red.Color("Hello, World!"))
+	if len(os.Args) < 3 {
+		fmt.Println("Incorrect Number Of Arguments Supplied")
+		os.Exit(1)
+	}
+	query := os.Args[1]
+	file_path := os.Args[2]
+	results := search(query, file_path)
+	for _, result := range results {
+		fmt.Println(result)
+	}
 }
